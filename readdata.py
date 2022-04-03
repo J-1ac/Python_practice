a00593ec1508365b2e176df40546c117ddfa242f
J-1ac <tkdwns26@naver.com>
2022-04-03 20:41:57 +0900

    commit by jun

--- readdata.py
+++ readdata.py
@@ -1,6 +1,7 @@
 f = open("data.txt", "r")
-while True:
-    line = f.readline()
-    if not line : break             #readline()은 더 이상 읽을 줄이 없으면 none 출력
+
+lines = f.readlines()
+for line in lines:
     print(line)
+    
 f.close()
