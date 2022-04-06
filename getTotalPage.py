29c6ce4c9868e95129507263343aa51dc1c9f772
J-1ac <tkdwns26@naver.com>
2022-04-06 22:45:17 +0900

    commit by jun

---
+++ getTotalPage.py
@@ -0,0 +1,11 @@
+#getTotalPage.py
+
+import math
+
+def getTotalPage(m, n):
+    return math.ceil(m/n)
+
+print(getTotalPage(5, 10))
+print(getTotalPage(15, 10))
+print(getTotalPage(25, 10))
+print(getTotalPage(30, 10))
