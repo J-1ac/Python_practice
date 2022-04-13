f000cf0b6249f82039d8989c8f8a88dc4f08e076
J-1ac <tkdwns26@naver.com>
2022-04-13 23:25:52 +0900

    commit by jun

--- python algorithm/1_13.py
+++ python algorithm/1_13.py
@@ -3,7 +3,7 @@
 print("+와 -를 번갈아 출력합니다.")
 n = int(input("몇 개를 출력할까요?: "))
 
-for _ in range(n//2):
+for _ in range(n//2):                   #for문에 _사용 -> 반환하는 값 사용 x, 무시하고 싶은 값 _로 표현 가능
     print("+-", end="")
 
 if n % 2:
