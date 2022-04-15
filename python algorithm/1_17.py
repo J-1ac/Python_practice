0fc53b2986e4c718643641ccf9255a8c54c2cee7
J-1ac <tkdwns26@naver.com>
2022-04-15 23:40:43 +0900

    commit by jun

--- python algorithm/1_17.py
+++ python algorithm/1_17.py
@@ -5,3 +5,4 @@ area = int(input("직사각형의 넓이를 입력하세요.: "))
 for i in range(1, area + 1):                            # 1부터 사각형의 넓이 계산
     if i*i > area: break
     if area % i: continue
+    print(f"{i} x {area // i}")
