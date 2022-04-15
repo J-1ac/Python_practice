8bf5510220fee161471a34e5840f782e8807b8f6
J-1ac <tkdwns26@naver.com>
2022-04-16 00:33:23 +0900

    commit by jun

--- python algorithm/1_20.py
+++ python algorithm/1_20.py
@@ -1,5 +1,5 @@
 # 1부터 12까지 8을 건너뛰고 출력하기 2
 
-for i in list(range(1, 8)) + list(range(9,13)):
+for i in list(range(1, 8)) + list(range(9,13)):         # 건너뛰는 값을 아는경우 사용 가능
     print(i, end=" ")
 print()
