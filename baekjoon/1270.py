a1c670180fd3bb766ce275d24c9a9f68cdc07dea
J-1ac <tkdwns26@naver.com>
2022-05-10 22:14:54 +0900

    commit

--- baekjoon/1270.py
+++ baekjoon/1270.py
@@ -1,2 +1,13 @@
+def consider_occupy(list_land):
+    population = list_land[0]
+    land = list_land[0:]
+    for i in range(population):
+        count = land.count(land[i])
+        if count > int(population/2):
+            return print(land[i])
+    return print("SYJKGW")
+
 T = int(input())
-land = 
+for _ in range(T):
+    land = list(map(int, input().split(" ")))
+    consider_occupy(land)
