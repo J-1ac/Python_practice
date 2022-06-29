7bb1a4c6dba55da4ea1171516972ac79164c4af0
J-1ac <tkdwns26@naver.com>
2022-06-30 00:03:31 +0900

    commit

--- baekjoon/1929.py
+++ baekjoon/1929.py
@@ -1,14 +1,24 @@
-def isprime(n):
-    if n==1:
-        return False
-    for i in range(2,n):
-        if n%i==0:
-            return False
-    return True
+import sys
 
-m, n=map(int,input().split())
+m, n=map(int,sys.stdin.readline().split())
 numlist=list(range(m,n+1))
-primenum=0
+primenumlist=[True]*len(numlist)
 for i in range(len(numlist)):
-    if isprime(numlist[i]):
+    isprimenum=True
+    if numlist[i]==1:
+        primenumlist[i]=False
+        continue
+    elif primenumlist[i]==True:
+        for j in range(2,int(numlist[i]**(1/2)+1)):
+            if numlist[i]%j==0:
+                isprimenum=False
+                primenumlist[i]=False
+                break
+        if isprimenum:
+            for k in range(numlist[i]*2-m,len(numlist),numlist[i]):
+                primenumlist[k]=False
+    else:
+        continue
+for i in range(len(numlist)):
+    if primenumlist[i]:
         print(numlist[i])
