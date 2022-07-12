ab658fbeec7f661cd796c67d8fd61993808bc81d
J-1ac <tkdwns26@naver.com>
2022-07-13 00:07:06 +0900

    commit

--- baekjoon/1268.py
+++ baekjoon/1268.py
@@ -1,19 +1,23 @@
 n=int(input())
 studentinfo=[]
-for _ in range(n):
-    studentinfo.append(list(map(int,input().split())))
 studentpoint=[0]*n
+max=0
+studentnum=1
+for i in range(n):
+    studentinfo.append(list(map(int,input().split())))
+    studentpoint[i]=[0]*n
+
 for t in range(5):
     for i in range(n-1):
         for j in range(i+1,n):
             if studentinfo[i][t]==studentinfo[j][t]:
-                studentpoint[i]+=1
-                studentpoint[j]+=1
-max=0
-studentnum=0
-print(studentpoint)
+                studentpoint[i][j]=1
+                studentpoint[j][i]=1
+cnt=[]
+for l in studentpoint:
+    cnt.append(l.count(1))
 for i in range(n):
-    if max<studentpoint[i]:
-        max=studentpoint[i]
+    if max<cnt[i]:
+        max=cnt[i]
         studentnum=i+1
 print(studentnum)
